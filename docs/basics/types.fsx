670b125b8b65a949154e1285ff25ff46e20643e3
Christian Sheridan <christiansheridan@outlook.com>
2022-06-28 22:35:32 -0500

    Update types.fsx

--- docs/basics/types.fsx
+++ docs/basics/types.fsx
@@ -9,11 +9,44 @@ index: 2
 
 (**
 # Types
-This is just a placeholder for now.
+
+## Primitives Types
+``int``: Represents numerical values from ``-2,147,483,648`` to ``2,147,483,647``.  
+``float``: Represents a floating point numerical value. Possible values range from: ``-1.79769313486232e308`` to ``1.79769313486232e308``.    
+``bool``: Represents a ``True`` or ``false`` value.  
+``char``: Represents a unicode character value.  
+``string``: Represents a sequence of ``char``s (unicode text).
 *)
 
-let numbers = [ 0..99 ];;
-(*** include-value: numbers ***)
+let integer = 1
+let float = 1.0
+let bool = true
+let char = 'a'
+let string = "abcdefg"
+(*** include-fsi-output ***)
 
-List.sum numbers;;
-(*** include-it ***)
+(**
+## Tuples
+Tuples are a finite ordered sequence of comma-separated values of differing types.
+The length and types of a tuple are known at compile-time and are present in its type signature.
+*)
+
+let coordinates = (1.0, 2.0, 3.0)
+(*** include-fsi-output ***)
+
+(**
+## Unit type
+Since every expression in F# must evaluate to a value,
+the unit type is used when no other value of interest exists.
+The unit type only has a single value: ``()``
+*)
+
+()
+(*** include-fsi-output ***)
+
+(**
+Often times, functions that perform an operation and don't have a useful value to return will instead return a unit value. 
+*)
+printfn "Hello, World!"
+(*** include-fsi-output ***)
+
