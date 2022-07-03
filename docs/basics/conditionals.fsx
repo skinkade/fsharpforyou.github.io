fa7ea17383bd7f7b0efccb9feabd34bde3f1b95e
Christian Sheridan <christiansheridan@outlook.com>
2022-07-03 16:36:57 -0500

    Add conditionals.fsx

---
+++ docs/basics/conditionals.fsx
@@ -0,0 +1,33 @@
+(**
+---
+title: Conditionals
+category: Basics
+categoryindex: 2
+index: 7
+---
+*)
+
+(**
+# Conditional Expressions
+Conditional expressions are expressions that evaluate a boolean value (true or false)
+and execute a conditional branch of code if that condition yields a ``true`` value: ``if X do Y, else do Z``
+*)
+
+let outputAge age =
+  if age >= 18 then printfn "You are an adult!"
+  else printfn "You are not an adult!"
+
+outputAge 23
+outputAge 15
+(*** include-output ***)
+
+(**
+All branches of a conditional expression must return values of the same type, which is inferred from the return value of the first branch (or optionally, the type annotation).
+If the first branch returns a ``unit`` value, the second branch cannot return a ``string`` value as ``string`` is not convertible to ``unit``.
+This code will result in a compiler error:
+```fsharp
+let outputAge' age =
+  if age >= 18 then "Adult" // <- string value
+  else () // <- unit value
+```
+*)
