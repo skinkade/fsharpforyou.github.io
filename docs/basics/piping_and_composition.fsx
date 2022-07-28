537ae269542de13a446e4be61c883e74af185245
Christian Sheridan <christiansheridan@outlook.com>
2022-07-27 23:10:06 -0500

    Update piping_and_composition.fsx
    Better examples
    Small adjustments to explanations
    Add note about partial application and conciseness

--- docs/basics/piping_and_composition.fsx
+++ docs/basics/piping_and_composition.fsx
@@ -11,20 +11,31 @@ index: 9
 (**
 ## Piping
 Piping allows you to pass values into a function's or union's parameter and return the result.
-This is often used when you're piping multiple function's and their results into each other.
+This is often used when you're piping multiple functions and their results into each other.
 
 Passing a value of ``10`` into a function like so:
 ```fsharp
 function 10
 ```
 
-Is equivalent to piping that value into the function like so:
+Is equivalent to piping the value into the function like so:
 ```fsharp
 10
 |> function
 ```
+As you can see, we're passing the value ``10`` into a single parameter function and producing the result.
+Because of this, combining the pipe operator with a partially applied function is incredibly useful.
 
-This is really beneficial when you're dealing with "long" function chains like so:
+```fsharp
+let add x y = x + y
+
+10
+|> add 5 // partially applying `5`
+|> add 7 // partially applying `7`
+|> add 9 // partially applying `9`
+```
+
+This is incredibly beneficial when you're dealing with "long" function chains like so:
 ```fsharp
 let f x = ...
 let g x = ...
@@ -47,24 +58,26 @@ let m x = ...
 |> m
 ```
 
-A more practical example of the pipe operator in action is this:
+A more practical example of the pipe operator would be passing an initial list through multiple List module functions in succession.
 *)
 let double x = x * 2
 let isEven x = x % 2 = 0
 
-let nums =
-    [0..20]
-    |> List.map double // double every number
-    |> List.filter isEven // only even numbers
-    |> List.sortDescending // sort from high to low
-(*** include-value: nums ***)
+[0..20]
+|> List.map double // double every number
+|> List.filter isEven // only even numbers
+|> List.sortDescending // sort from high to low
+(*** include-it ***)
 
 (**
+We can add or remove steps from the above pipeline by inserting or removing a pipe.
+This allows us to construct elegant and concise workflows and easily reason about them.
+
 ## Function Composition
 
 You can compose multiple functions together using the ``>>`` operator.
 This allows you to evaluate the left-hand function and pass the result into the right-hand function.
-The output type of the left-hand function must be the input type of the right hand function, example: ``A -> B`` and ``B -> C``.
+The output type of the left-hand function must be the input type of the right-hand function, for example: ``A -> B`` and ``B -> C``.
 This is very similar to piping, but instead of piping values into functions, we are building up new functions by combining smaller ones.
 *)
 let addFive x = x + 5
@@ -75,7 +88,7 @@ let twenty = addFiveMultiplyTwo 5
 (*** include-fsi-output ***)
 
 (**
-You can chain these together to create new function by composing (or combining) many smaller functions
+You can chain these together to create new functions by composing (or combining) many smaller functions together.
 *)
 let addTwo x = x + 2
 let multiplyFive x = x * 5
