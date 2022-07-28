5b846e7b15cf760e52861c627b6a6d00fac90623
Christian Sheridan <christiansheridan@outlook.com>
2022-07-27 22:31:32 -0500

    Update fundamental_types.fsx
    Small additions.

--- docs/basics/fundamental_types.fsx
+++ docs/basics/fundamental_types.fsx
@@ -9,10 +9,13 @@ index: 2
 
 (**
 # Fundamental Types
+Every value in F# has a type. Types describe the kind of value something is and what we can do with it.
 
 ## Primitives Types
+Primitive types are the most basic types in F# and they form the basis of nearly every program.
+
 ``int``: Represents numerical values from ``-2,147,483,648`` to ``2,147,483,647``.  
-``float``: Represents a floating point numerical value. Possible values range from: ``-1.79769313486232e308`` to ``1.79769313486232e308``.    
+``float``: Represents a 64-bit floating point numerical value. Possible values range from: ``-1.79769313486232e308`` to ``1.79769313486232e308``.    
 ``bool``: Represents a ``True`` or ``false`` value.  
 ``char``: Represents a unicode character value.  
 ``string``: Represents a sequence of ``char``s (unicode text).
@@ -36,7 +39,7 @@ let coordinates = (1.0, 2.0, 3.0)
 
 (**
 ## Unit type
-Since every expression in F# must evaluate to a value,
+Since every expression in F# must yield or produce a value,
 the unit type is used when no other value of interest exists.
 The unit type only has a single value: ``()``
 *)
@@ -45,8 +48,7 @@ The unit type only has a single value: ``()``
 (*** include-fsi-output ***)
 
 (**
-Often times, functions that perform an operation and don't have a useful value to return will instead return a unit value. 
+Oftentimes, functions that perform an operation and don't have a useful value to return will instead return a unit value. 
 *)
 printfn "Hello, World!"
 (*** include-fsi-output ***)
-
