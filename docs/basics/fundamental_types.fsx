44e3c1e6dfecd4f09ee92f56545564bb90626141
Christian Sheridan <christiansheridan@outlook.com>
2022-08-12 22:38:50 -0500

    Update primitive types

--- docs/basics/fundamental_types.fsx
+++ docs/basics/fundamental_types.fsx
@@ -15,8 +15,9 @@ Every value in F# has a type. Types describe the kind of value something is and 
 Primitive types are the most basic types in F# and they form the basis of nearly every program.
 
 `int`: Represents numerical values from `-2,147,483,648` to `2,147,483,647`.  
-`float`: Represents a 64-bit floating point numerical value. Possible values range from: `-1.79769313486232e308` to `1.79769313486232e308`.    
-`bool`: Represents a `True` or `false` value.  
+`float`, `double`: Represents a 64-bit double-precision floating point number. Possible values range from: `-1.79769313486232e308` to `1.79769313486232e308`.    
+`float32`, `single`: Represents a 32-bit single-precision floating point number. Possible values range from: `-3.402823e38` to `3.402823e38`.  
+`bool`: Represents a `true` or `false` value.  
 `char`: Represents a unicode character value.  
 `string`: Represents a sequence of `char`s (unicode text).
 *)
